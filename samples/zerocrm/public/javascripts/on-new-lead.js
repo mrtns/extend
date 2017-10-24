175e76534a9c75adadf71b8e2546b6f70a0cc91b
Glenn Block <glenn.block@gmail.com>
2017-10-24 20:32:46 -0300

    Adding zerocrm-compiler, updating code, and also enabling editor full screen

--- samples/zerocrm/public/javascripts/on-new-lead.js
+++ samples/zerocrm/public/javascripts/on-new-lead.js
@@ -1,22 +1,48 @@
-// This code will execute whenever a new lead is created.
-// Use 1000+ Node.js modules here. 
+// This code will enrich the lead with additional profile information whenever a new lead is created
+// Require any Node.js modules that you added here.',
+// You can access secrets using: module.exports.secrets',
+module.exports = function(lead, cb) {
+  console.log('On new lead:', lead);
 
-module.exports = function(ctx, cb) {
-  console.log('On new lead:', ctx.body);
+  lead.profile = {
+    created: new Date(),
+    vip: (lead.value > 1000)
+  };
   
-  var lead = ctx.body;
+  // To use Clearbit add a clearbit_key secret with your clearbit key as the value.',
+  // You can signup at https://dashboard.clearbit.com/signup',
+  var clearbit_key = module.exports.secrets.clearbit_key; 
   
-  if (lead.value > 1000) {
-    // Send e-mail to manager
-    // ...
+  // If the clearbit_key secret has been set',
+  if (clearbit_key !== undefined) {
+    getProfileFromClearbit(lead.email, (err, clearbit) => {
+      if(!err) {
+       lead.profile.clearbit = clearbit;
+       cb(null, lead);
+      }
+      else {
+        cb(err);
+      }
+    });
+  }
+  else {
+    cb(null, lead);
   }
 
-  // lead.profile = getProfileFromFullContact(lead.email);
-  lead.profile = {
-    vip: true,
-    comment: 'This was added by custom code'
-  };
- 
-  // return the newly created lead
-  cb(null, lead);
+  // Calls clearbit to get social media information for the lead',
+  function getProfileFromClearbit(email, cb) {
+    var clearbit = require("clearbit")(module.exports.secrets.clearbit_key);
+    var person = clearbit.Person;
+    var cb_profile = {}
+    person.find({email: email}).
+      then(person=> {
+        cb_profile.github = person.github.handle;
+        cb_profile.twitter = person.twitter.handle;
+        cb_profile.linkedin = person.linkedin.handle;
+        cb(null, cb_profile);
+      }).
+      catch(e=> {
+        cb(err);
+      });
+   }
 };
--- samples/zerocrm/public/javascripts/on-opportunity-changed.js
+++ samples/zerocrm/public/javascripts/on-opportunity-changed.js
@@ -1,13 +1,12 @@
 // This code will execute whenever an opportunity is changed.
 // Use 1000+ Node.js modules here. 
 
-module.exports = function(ctx, cb) {
-  console.log('On opportunity changed:', ctx.body);
-  var opportunity = ctx.body;
-  
-  if (opportunity.size > 1000) {
+module.exports = function(oppty, cb) {
+  console.log('On opportunity changed:', oppty);
+
+  if (oppty.size > 1000) {
     // send e-mail to manager
   }
   
-  cb(null, opportunity);
+  cb(null, oppty);
 };
